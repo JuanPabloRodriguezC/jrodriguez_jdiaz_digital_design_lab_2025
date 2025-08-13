5ab9a0bfcbaa47da40e741abf89f982570198e14
JafetDM <148803642+JafetDM@users.noreply.github.com>
2025-08-13 13:40:05 -0600

    arreglo errores restador 1 bit y tb

--- laboratorio_2/restadorCompleto1bit.vhd
+++ laboratorio_2/restadorCompleto1bit.vhd
@@ -2,15 +2,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all; -- importa librerias
 
 entity restadorCompleto1bit is -- entidad que forma parte de la estructura grande
-//realiza la operacion B - A con un bit de acarreo Cin
+--realiza la operacion B - A con un bit de acarreo Cin. B es el minuendo, A el sustraendo
     port (
         A, B, Cin : in std_logic; --in de input
         S, Cout : out std_logic --out de output
-    )
+    );
+end;
 
     architecture Behavioral of restadorCompleto1bit is -- arquitectura de la entidad (lo que hace el modulo)
     begin
-        Y <= ((B xor A) xor Cin); -- salida S es la suma xor de A, B y Cin (ver diseño)
+        S <= ((B xor A) xor Cin); -- salida S es la suma xor de A, B y Cin (ver diseño)
 
-        Cout <= ((Cin and not(B)) or (Cin and A) or (not(B) or A)); -- segun la ecuacion determinada (ver diseño)
+        Cout <= ((Cin and not(B)) or (Cin and A) or (not(B) and A)); -- segun la ecuacion determinada (ver diseño)
     end Behavioral; -- fin de la arquitectura
--- laboratorio_2/restadorCompleto1bit_tb.vhd
+++ laboratorio_2/restadorCompleto1bit_tb.vhd
@@ -1,5 +1,5 @@
 library IEEE; 
-use IEEE.STD_LOGIC_1164.all; // importar librerias
+use IEEE.STD_LOGIC_1164.all; -- importar librerias
 
 entity restadorCompleto1bit_tb is 
 end;
@@ -7,17 +7,17 @@ end;
 architecture sim of restadorCompleto1bit_tb is
 component restadorCompleto1bit
 port (
-        A, B, Cin : in std_logic; //in de input
-        S, Cout : out std_logic //out de output
-    )
-end component //copiamos los puertos
+        A, B, Cin : in std_logic; --in de input
+        S, Cout : out std_logic --out de output
+    );
+end component; --referenciamos los puertos a probar
 
-signal AS, BS, CinS: std_logic; // declaramos las señales
-signal SS, CoutS: std_logic; // declaramos las señales
+signal AS, BS, CinS: std_logic; -- declaramos las señales
+signal SS, CoutS: std_logic; -- declaramos las señales
 
 
 begin
-// instantiate device under test
+-- instancia device under test
 dut: restadorCompleto1bit port map(
 
         A => AS,
@@ -25,17 +25,22 @@ dut: restadorCompleto1bit port map(
         Cin => CinS,
         S => SS,
         Cout => CoutS
-    ); // definimos las igualdades de las señales
+    ); -- definimos las igualdades de las señales
 
 process 
     begin 
+	 
+	     AS <= '0'; BS <= '0'; CinS <= '0'; -- inicializamos las señales
+		  wait for 20 ns; -- esperamos 20 ns
+        AS <= '1'; BS <= '0'; CinS <= '0'; -- cambiamos las señales
+		  wait for 20 ns; 
+        AS <= '0'; BS <= '1'; CinS <= '0'; wait for 20 ns;
+        AS <= '1'; BS <= '1'; CinS <= '0'; wait for 20 ns;
+        AS <= '0'; BS <= '0'; CinS <= '1'; wait for 20 ns;
+        AS <= '1'; BS <= '0'; CinS <= '1'; wait for 20 ns;
+        AS <= '0'; BS <= '1'; CinS <= '1'; wait for 20 ns;
+        AS <= '1'; BS <= '1'; CinS <= '1'; wait for 20 ns;
 
-    AS <= '0'; BS <= '0'; CinS <= '0'; // inicializamos las señales
-    wait for 20 ns; // esperamos 20 ns
-    AS <= '0'; BS <= '0'; CinS <= '1'; // cambiamos las señales
-    wait for 20 ns; // esperamos 20 ns
-    AS <= '0'; BS <= '1'; CinS <= '0'; // cambiamos las señales
-    wait for 20 ns; // esperamos 20 ns
-    AS <= '1'; BS <= '1'; CinS <= '1'; // cambiamos la señal
-    wait for 20 ns; // esperamos 20 ns  
+		wait;
+	end process;
 end;
