317b544930f3cbf53e112dd388688252a3ecc512
JafetDM <148803642+JafetDM@users.noreply.github.com>
2025-08-20 11:37:10 -0600

    agrega debounce

--- laboratorio_1/problema_3.sv
+++ laboratorio_1/problema_3.sv
@@ -1,27 +1,32 @@
-module problema_3 #(parameter WIDTH=8)(
-	input logic clk,
-	input logic reset_n,
-	input logic enable,
-	output logic [WIDTH-1:0] count,
-	output logic overflow
+module problema_3 #(parameter WIDTH=4)(
+    input  logic clk,
+    input  logic reset_n,
+    input  logic button,
+    output logic [6:0] segments
 );
-	always_ff @(posedge clk or negedge reset_n) begin
-		if (!reset_n) begin
-			count <= '0;
-			overflow <= 1'b0;
-		end
-		if (enable) begin
-			if (count == (2**WIDTH-1)) begin
-				count <= '0;
-				overflow <= 1'b1;
-			end
-			else begin
-				count <= count + 1'b1;
-				overflow <= 1'b0;
-			end
-		end
-		else begin
-			overflow <= 1'b0;
-		end
-	end
+    logic [WIDTH-1:0] count;
+    logic overflow;
+    logic button_pulse;
+
+    button_edge debouncer (
+        .clk(clk),
+        .reset_n(reset_n),
+        .button_in(button),
+        .button_pulse(button_pulse)
+    );
+
+    // 2. Contador parametrizable
+    contador #(.WIDTH(WIDTH)) contador (
+        .clk(clk),
+        .reset_n(reset_n),
+        .enable(button_pulse),
+        .count(count),
+        .overflow(overflow)
+    );
+
+    // 3. Decoder a display 7 segmentos
+   decoder_segmentos decoder (
+        .data(count),
+        .segments(segments)
+    );
 endmodule
--- laboratorio_1/problema_3_tb.sv
+++ laboratorio_1/problema_3_tb.sv
@@ -1,37 +1,66 @@
+timescale 1ns/1ps
+
 module problema_3_tb;
-	logic clk;
-	logic reset_n;
-	logic enable;
-	logic[7:0] count;
-	logic overflow;
-	
-	problema_3 #(.WIDTH(8)) dut (
-		.clk(clk),
-		.reset_n(reset_n),
-		.enable(enable),
-		.count(count),
-		.overflow(overflow)
-	);
-	
-	initial begin
-		clk=0;
-		forever #5 clk = ~clk;
-	end
-	
-	initial begin
-		reset_n = 0;
-		enable = 0;
-		
-		#20
-		reset_n = 1;
-		#20
-		enable = 1;
-		#2550;
-		reset_n = 0;
-		#20
-		reset_n = 1;
-		
-		#100 
-		$finish;
-	end
+    parameter WIDTH = 4;
+
+    logic clk;
+    logic reset_n;
+    logic button;
+    logic [6:0] segments;
+
+    
+    problema_3 #(.WIDTH(WIDTH)) dut (
+        .clk(clk),
+        .reset_n(reset_n),
+        .button(button),
+        .segments(segments)
+    );
+
+    // Generador de reloj (100 MHz → periodo 10 ns)
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        reset_n = 0;
+        button  = 0;
+        #20;
+
+        reset_n = 1;
+        #20;
+
+        press_button();
+
+        #50;
+     
+        press_button();
+
+        #50;
+        
+        bounce_button();
+
+        #200;
+
+ 
+    end
+
+    // Tarea: pulsación limpia
+    task press_button;
+        begin
+            button = 1;
+            #15;   // mantener 15 ns
+            button = 0;
+        end
+    endtask
+
+    task bounce_button;
+        begin
+            button = 1; #5;
+            button = 0; #5;
+            button = 1; #5;
+            button = 0; #5;
+            button = 1; #10;
+            button = 0;
+        end
+    endtask
+
 endmodule
